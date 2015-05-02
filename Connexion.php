c4a98205a9251d734f1245ecf39ab4007e452666
FrogEnjoy <zoe.deschamps@hotmail.fr>
2015-05-02 19:02:37 +0200

    Création php
    - Création d'une base php
    - Création du système de texte dans "Fantastique"
    - Essai pour l'inscription

---
+++ Connexion.php
@@ -0,0 +1,80 @@
+<?php 
+include ("PageHaut.php");
+?>
+
+<link rel="stylesheet" href="Connexion.css" />
+
+		<div>
+			<tbody >
+				<form method="post" >
+				<fieldset>
+					<legend>Connexion</legend>
+						<table>
+						<label>Pseudo     :      </label> <input id="name" type="text" name="pseudo"/><br/>
+						<label>Mot de Passe :    </label> <input id="mdp" type="password" name="mdp"/><br/>
+						</table>
+				</fieldset>
+			</form>
+			
+				<input type=submit value=Connexion> <input type=submit value="Mdp Perdu ?">
+				
+			<form>	
+				<fieldset>
+					<legend>Inscription</legend>
+						<table>
+						<label>Pseudo     :     </label> <input type="text" name="pseudo"/><br/>
+						<label>Mot de Passe :   </label> <input type="password" name="mdp"/><br/>
+						<label>Adresse Mail:    </label> <input type="text" name="email"/><br/>
+						</table>
+				</fieldset>
+				<br> <input type="submit" name="valider"value="Inscription"/>
+			</form>
+			</tbody>
+			
+<?php
+  
+  if(isset($_POST['valider']))
+    {
+         // D'abord, je me connecte à la base de données.
+     $bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
+    
+      
+      // Je recupere les infos, plus securité pour le code.
+		htmlentities($pseudo = $_POST['pseudo']);
+        htmlentities($mdp = $_POST['mdp']);
+        htmlentities($email = $_POST['email']);
+      
+       // empecher les codes php dans la base
+      // Je verifie que TOUT les champs sont remplis.
+      if(empty($pseudo)||empty($mdp)||empty($email) )
+      {
+        echo'Vous devez remplir toutes les coordonnées';
+      }
+      else
+      {
+          $result = $bdd->query("SELECT email FROM utilisateur WHERE email = $email");
+          $result2 = $result -> fetch();
+          if (empty($result2['email'])){
+              if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $reponse = $bdd->query("INSERT INTO utilisateur VALUES('', '$pseudo','$email', '$mdp')");
+                      //affiche un mot gentil, dans le futur on doit changer pour que ceci apparaisse sur une autre.
+                      echo "Bonjour $pseudo votre compte est bien enregistré";
+                } else {
+                  echo 'Votre email n\'est pas valide';
+              }
+          }
+          else{
+           echo 'votre email est déjà utilisé.';
+          }
+      }
+    }
+    
+?>
+		
+		</div> 
+
+		
+		
+<?php 
+include ("PageBas.php");
+?>
---
+++ Fantastique.php
@@ -0,0 +1,73 @@
+<?php 
+	include ("PageHaut.php");
+?>
+
+<link rel="stylesheet" href="Fantastique.css" />
+
+
+
+<?php
+try
+{
+	// On se connecte à MySQL
+	$bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM Fantastique INNER JOIN Utilisateur ON Utilisateur_idUtilisateur=idUtilisateur ORDER BY Date_Fanta DESC');
+
+
+// On affiche chaque entrée une à une
+while ($donnees = $reponse->fetch())
+{
+?>
+
+
+<center><table cellspacing="0" cellpadding="0" border="0" width="680" >
+		<tr>
+		<td>
+			<table cellspacing="0" cellpadding="0" border="1" width="674">
+				<tr>
+					<td align=center><?php echo $donnees['Pseudo']; ?></td>
+					<td align=center><?php echo $donnees['Nom_Fanta']; ?></td>
+					<td align=center><?php echo $donnees['Date_Fanta']; ?></td>
+				</tr>
+			</table>
+		</td>
+		</tr>
+		
+		<tr>
+		<td>
+			<div style="width:700px; height:200px; overflow:auto;">
+			<table cellspacing="0" cellpadding="1" border="1" width="674">
+				<tr>
+					<td colspan=3 align=center style="overflow:auto"><?php echo $donnees['Texte_Fanta']; ?></td>
+				<tr>
+			</table>
+			</div>
+		</td>
+		</tr>
+</table></center>
+</br>
+	
+   
+<?php
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
+<!--http://stackoverflow.com/questions/14834198/table-scroll-with-html-and-css-->
+
+<?php 
+	include ("PageBas.php");
+?>
+
