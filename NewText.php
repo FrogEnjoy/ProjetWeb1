136af412d6201aa8d1016c0a9666203c6a87262c
Quentin <quentin.lalle@gmail.com>
2015-05-12 19:54:50 +0200

    Connection BDD avec NewText
    Pour l'instant ça ne fonctionne pas, mais je vais regarder ça

--- NewText.php
+++ NewText.php
@@ -6,36 +6,66 @@
 		
 		
 		<div>
-			<form>
+			<form method="post">
 				<table>
 					<tr><td>Pseudo:
 					<input id="name" type="text" name="pseudo"/></td></tr>
 					<tr><td>Mot de Passe:
 					<input id="mdp" type="password" name="mdp"/></td></tr>
-					<tr><td colspan=2>Essai:<br>
-					<textarea COLS=100 ROWS=10 name=essai></textarea>
+					<tr><td colspan="2">Essai:<br>
+					<textarea COLS="100" ROWS="10" name="essai"></textarea>
 					</td></tr>
 				</table>
 				<table>
 					<tr><td>Thème:
 						<form method="POST" name="menu" >
-							<select name="selecttheme" onChange="">
+							<select name="selecttheme">
 								<option> </option>
-								<option>Thriller</option>
-								<option>Fantastique</option>
-								<option>Science-Fiction</option>
-								<option>Horreur</option>
-								<option>Romantique</option>
+								<option value="Thriller">Thriller</option>
+								<option value="Fantastique">Fantastique</option>
+								<option value="Science-Fiction">Science-Fiction</option>
+								<option value="Horreur">Horreur</option>
+								<option value="Romantique">Romantique</option>
 							</select>
 						</form>
 					</td></tr>
 				</table>
-				<br> <input type=submit value=Envoyer><input type=submit value=Annuler>
+				<br> <input type="submit" value="Envoyer" name="valider">
+				<input type="submit" value="Annuler" name="retour">
 			</form>
 		</div> 
+
+
+
+
+<?php
+
+//ne fonctionne pas pour le moment
+
+//fonctionne seulement quand le boutton est activé
+if(isset($_POST["valider"]))
+	
+		$bdd = new PDO("mysql:host=localhost;dbname=zoe;charset=utf8", "root", "");
+	
+	
+
+
+	htmlentities($essai=$_POST["essai"]);
+       
+
+		if ($_POST["selecttheme"] == "Fantastique"){
+			$reponse = $bdd->query("INSERT INTO 'zoe'.'fantastique'('Texte_Fanta') VALUES($essai)");
+
+		}else if ($_POST["selecttheme"] == "Thriller"){
+			$reponse = $bdd->query("INSERT INTO 'zoe'.'thriller'('Texte_Thriller') VALUES($essai)");
+		}
+	
+?>
 
 
 
 <?php
 	include("PageBas.php");
 ?>
+
+<!--Je fais un essai pour voir si ça fonctionne bien-->
