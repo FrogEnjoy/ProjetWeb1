8cfc5a5c720e5039261a65a9f2c3c528d22d774f
FrogEnjoy <zoe.deschamps@hotmail.fr>
2015-05-12 20:41:33 +0200

    ajout
    2, 3 ajouts
    j'ai ajouté des pages et des photos => encore à modifier un peu, certain
    sont des test, ne pas faire attention

---
+++ Romantique.php
@@ -0,0 +1,74 @@
+<?php 
+	include ("PageHaut.php");
+?>
+
+<link rel="stylesheet" href="Romantique.css" />
+
+<img id="bisous" src="bisous.gif"/> <br>
+<a id="new" href="NewText.php">Transmettre un Essai</a><br>
+
+<?php
+try
+{
+	// On se connecte à MySQL
+	$bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM Romantique INNER JOIN Utilisateur ON Utilisateur_idUtilisateur=idUtilisateur ORDER BY Date_Roman DESC');
+
+
+// On affiche chaque entrée une à une
+while ($donnees = $reponse->fetch())
+{
+?>
+
+
+<center><table cellspacing="0" cellpadding="0" border="0" width="680">
+		<tr>
+		<td>
+			<table cellspacing="0" cellpadding="0" border="1" width="674">
+				<tr>
+					<td align=center><?php echo $donnees['Pseudo']; ?></td>
+					<td align=center><?php echo $donnees['Nom_Roman']; ?></td>
+					<td align=center><?php echo $donnees['Date_Roman']; ?></td>
+				</tr>
+			</table>
+		</td>
+		</tr>
+		
+		<tr>
+		<td>
+			<div style="width:700px; height:200px; overflow:auto;">
+			<table cellspacing="0" cellpadding="1" border="1" width="674">
+				<tr>
+					<td colspan=3 align=center style="overflow:auto"><?php echo $donnees['Texte_Roman']; ?></td>
+				<tr>
+			</table>
+			</div>
+		</td>
+		</tr>
+</table></center>
+</br>
+	
+   
+<?php
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
+<!--http://stackoverflow.com/questions/14834198/table-scroll-with-html-and-css-->
+
+<?php 
+	include ("PageBas.php");
+?>
+
