91166812442ebfd0df55d8c0c53a46fd206278b2
Quentin <quentin.lalle@gmail.com>
2015-05-19 22:05:03 +0200

    Rajout commentaire sur la page

--- Connexion.php
+++ Connexion.php
@@ -18,20 +18,23 @@ include ("PageHaut.php");
 			
 				<input type=submit value=Connexion> <input type=submit value="Mdp Perdu ?">-->
 				
-			<form action="" method="POST">	
-				<fieldset>
-					<legend>Inscription</legend>
-						<table>
-						<p>   Pseudo     :      <input type="text" name="Pseudo"/><br/>
-						      Mot de Passe :    <input type="password" name="Mdp"/><br/>
-						      Adresse Mail:     <input type="text" name="email"/><br/>
-            </p>
-						</table>
-				</fieldset>
-				<br> <input type="submit" name="valider"value="Inscription"/>
+  			<form action="" method="POST">	
+  				<fieldset>
+  					<legend>Inscription</legend>
+  						<table>
+  						<p>   Pseudo     :      <input type="text" name="Pseudo"/><br/>
+  						      Mot de Passe :    <input type="password" name="Mdp"/><br/>
+  						      Adresse Mail:     <input type="text" name="email"/><br/>
+              </p>
+  						</table>
+  				</fieldset>
+  				<br> <input type="submit" name="valider"value="Inscription"/>
+        </form>
+      </tbody>
 			
 			
 <?php
+  //vérifie que tout soit remplis afin de faire la requete
   if(isset($_POST["Pseudo"])&& 
     isset($_POST["Mdp"])&& 
     isset($_POST["email"])
@@ -41,10 +44,12 @@ include ("PageHaut.php");
     // D'abord, je me connecte à la base de données.
    try{     
     $bdd = new PDO("mysql:host=localhost;dbname=mydb;charset=utf8", "root", "");
+    //donne ma requete SQL
     $sql = "INSERT INTO `mydb`.`utilisateur` (`Pseudo`,`Mdp`, `email`) VALUES (:Pseudo, :Mdp, :email)";
+    //prépare la requete
     $request = $bdd->prepare($sql);
 
-    
+    //o met dans un tableau les variables
     $request->execute(array(
     "Pseudo" => $_POST["Pseudo"],
     "Mdp" => $_POST["Mdp"],
@@ -54,15 +59,14 @@ include ("PageHaut.php");
     catch(PDOException $e) {echo $e->getMessage();}
 
 
-      //print_r($_POST);
+      
       //affiche un mot gentil, dans le futur on doit changer pour que ceci apparaisse sur une autre.
       echo "Bonjour " .$_POST["Pseudo"]. " votre compte est bien enregistré !";
   } 
   
 
 ?>
-		</form>
-      </tbody>
+		
 		
 <?php 
 include ("PageBas.php");
